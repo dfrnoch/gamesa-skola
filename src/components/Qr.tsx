7df677f0e91a942ab91ef456a8a3b7e4ba809ecb
lnxcz <frnoch@pm.me>
2023-02-21 10:11:36 +0100

    feat: :sparkles: stavniste

--- src/components/Qr.tsx
+++ src/components/Qr.tsx
@@ -1,26 +1,28 @@
 import React, { useState } from "react";
 import { QrReader } from "react-qr-reader";
 
-export default function QR() {
-  const [data, setData] = useState("Nic");
+export default function CodeReader() {
+  const [data, setData] = useState("");
 
   return (
-    <>
-      <QrReader
-        onResult={(result, error) => {
-          if (result) {
-            setData(result.getText());
-          }
+    <div className="absolute top-0 left-0 w-screen h-screen">
+      <div className="">
+        <QrReader
+          onResult={(result, error) => {
+            if (result) {
+              setData(result.getText());
+            }
 
-          if (!error) {
-            console.info(error);
-          }
-        }}
-        constraints={{
-          facingMode: "environment",
-        }}
-      />
-      <p>{data}</p>
-    </>
+            if (!error) {
+              console.info(error);
+            }
+          }}
+          constraints={{
+            facingMode: "environment",
+          }}
+        />
+        <p>{data}</p>
+      </div>
+    </div>
   );
 }
