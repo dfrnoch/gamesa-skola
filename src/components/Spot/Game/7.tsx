0c4f8df17a5e4a6c5eb5d8f92e4ee0d0a61b5a6e
SsaiboT <tobisokola@seznam.cz>
2023-02-22 11:49:54 +0100

    rdfhcg

--- src/components/Spot/Game/7.tsx
+++ src/components/Spot/Game/7.tsx
@@ -3,14 +3,12 @@ import { TouchBackend } from "react-dnd-touch-backend";
 import { DndProvider } from "react-dnd";
 
 function Seventh() {
-  const ItemTypes = {
-    KNIGHT: "knight",
-  };
-
   return (
     <div className=" bg-gray-700 w-screen h-screen text-gray-200">
+      <DndProvider backend={TouchBackend}>
+        <span>♘</span>
+      </DndProvider>
       <h1>cau</h1>
-      <span>♘</span>
     </div>
   );
 }
