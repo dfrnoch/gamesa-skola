d988f87236ffa97ea24ef625735726a2dfcd44a1
SsaiboT <tobisokola@seznam.cz>
2023-02-22 12:08:11 +0100

    bing chilling

--- src/components/Spot/Game/1.tsx
+++ src/components/Spot/Game/1.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Fist() {
-  return <div>1</div>;
+  return (
+    <div className=" bg-gray-700 w-screen h-screen text-gray-200">
+      <h1>Chceš se vyléčit ze zombie nákazy?</h1>
+    </div>
+  );
 }
