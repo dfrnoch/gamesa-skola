be4186e4c3e9029f2162f6e860a380ca95ac1a78
lnxcz <frnoch@pm.me>
2023-02-22 13:37:07 +0100

    feat: :sparkles: add support for selecting answer in SelectAnswer component

--- src/components/SelectAnswer.tsx
+++ src/components/SelectAnswer.tsx
@@ -5,13 +5,20 @@ interface Props {
   }[];
   handleSelect: (letter: string) => void;
 }
-export default function SelectAnswer({ data }: Props) {
+export default function SelectAnswer({ data, handleSelect }: Props) {
   return (
     <div>
-      {data.map((item, index) => (
-        <div key={item} className=" flex justify-between">
-          <div>a</div>
-          <div className="border-[2px]">{item}</div>
+      {data.map((item) => (
+        // rome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+        <div
+          key={item.letter}
+          className="cursor-pointer flex justify-start items-center gap-20 "
+          onClick={() => {
+            handleSelect(item.letter);
+          }}
+        >
+          <div>{item.letter}</div>
+          <div className="border-[2px]">{item.question}</div>
         </div>
       ))}
     </div>
--- src/components/Spot/Game/2.tsx
+++ src/components/Spot/Game/2.tsx
@@ -2,14 +2,31 @@ import React, { useState, ChangeEvent } from "react";
 import SelectAnswer from "~/components/SelectAnswer";
 
 export default function Third() {
+  const [questions, setQuestions] = useState([
+    {
+      question: "Co je hlavní součást antivirotik",
+      letter: "A",
+    },
+    {
+      question: "Co ssffadcadcwad",
+      letter: "B",
+    },
+  ]);
+  const [answer, setAnswer] = useState("");
+
+  const handleSelect = (letter: string) => {
+    setAnswer(letter);
+  };
+
   return (
     <div className=" bg-gray-700 w-screen h-screen text-gray-200">
       <div className="py-10">
         <h1 className=" text-4xl text-center">Co je hlavní součást antivirotik</h1>
       </div>
       <div className="px-20 mt-16 text-3xl">
-        <SelectAnswer data={["asdwa", "sadw"]} />
+        <SelectAnswer data={questions} handleSelect={handleSelect} />
       </div>
+      odpoved: {answer}
     </div>
   );
 }
