85cb95c090a99fa05cf85dd553da433ed26f9304
lnxcz <frnoch@pm.me>
2023-02-23 10:09:06 +0100

    revert: :rewind: revert SpotGame number to match currentSpot.number

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
   return (
     <main className="h-screen w-screen flex-col bg-gray-200">
       {openQr && <CodeReader />}
-      {!getGame.data.completedSpot && <SpotGame number={3} />}
+      {!getGame.data.completedSpot && <SpotGame number={getGame.data.currentSpot.number} />}
 
       <div className="flex flex-col w-screen h-screen justify-between">
         <Mapa />
