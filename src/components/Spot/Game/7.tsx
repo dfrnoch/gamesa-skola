158deaa8184eabc58405e1ac6de221301026038e
lnxcz <frnoch@pm.me>
2023-02-23 12:09:56 +0100

    fix: :bug: fix useState and handleClick for Seventh component in Spot/Game

--- src/components/Spot/Game/7.tsx
+++ src/components/Spot/Game/7.tsx
@@ -1,31 +1,49 @@
+import { useState } from "react";
+
 export default function Seventh() {
-  function handleClick() {}
+  const [letters, setLetters] = useState([
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+  ]);
+  const [selected, setSelected] = useState<string[]>([]);
+
+  function handleClick(letter: string) {
+    setSelected([...selected, letter]);
+    setLetters(letters.filter((l) => l !== letter));
+  }
 
   return (
     <>
       <div className="flex justify-center">
-        <div>
-          <div className="flex justify-center">
-            <button onClick={() => handleClick}>R</button>
-            <button onClick={() => handleClick}>Ch</button>
-            <button onClick={() => handleClick}>I</button>
-            <button onClick={() => handleClick}>Ý</button>
-          </div>
-          <div className="flex justify-center">
-            <button onClick={() => handleClick}>H</button>
-            <button onClick={() => handleClick}>O</button>
-            <button onClick={() => handleClick}>V</button>
-            <button onClick={() => handleClick}>A</button>
-          </div>
-          <div className="flex justify-center">
-            <button onClick={() => handleClick}>R</button>
-            <button onClick={() => handleClick}>P</button>
-            <button onClick={() => handleClick}>E</button>
-            <button onClick={() => handleClick}>N</button>
-          </div>
-          <div className="flex justify-center">
-            <button onClick={() => handleClick}>O</button>
-            <button onClick={() => handleClick}>T</button>
+        <div className="flex flex-row flex-wrap gap-10 items-center justify-center">
+          {letters.map((letter) => (
+            // rome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+            <div
+              key={letter}
+              className="flex justify-center items-center gap-10 mt-4 font-semibold"
+              onClick={() => handleClick(letter)}
+            >
+              <div className="border-2 border-dashed p-1 rounded-xl border-red-800">
+                <div className="text-center px-5 py-2 rounded-lg bg-red-800">{letter}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center items-center gap-10 mt-4 font-semibold">
+          <div className="border-2 border-dashed p-">
+            <div className="text-center px-5 py-2 rounded-lg bg-red-800">{selected.join("")}</div>
           </div>
         </div>
       </div>
