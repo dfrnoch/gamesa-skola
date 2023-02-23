8afe392697b43dbf9d841a75f509675ca6136854
SsaiboT <tobisokola@seznam.cz>
2023-02-23 11:16:50 +0100

    vypuzzlil jsem že se máš zabít hehehe ha

--- src/components/Spot/Game/2.tsx
+++ src/components/Spot/Game/2.tsx
@@ -1,3 +1,15 @@
+import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
+import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
+
 export default function Second() {
-  return <div>2</div>;
+  return (
+    <div className="border-2 mt-10">
+      <JigsawPuzzle
+        imageSrc='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+        rows={3}
+        columns={3}
+        onSolved={() => alert("Solved!")}
+      />
+    </div>
+  );
 }
