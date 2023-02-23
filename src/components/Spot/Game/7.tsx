2fdcddffd1b42f0d9722f56e2af3545f5110392d
SsaiboT <tobisokola@seznam.cz>
2023-02-23 11:56:53 +0100

    fuck you i am smart

--- src/components/Spot/Game/7.tsx
+++ src/components/Spot/Game/7.tsx
@@ -2,31 +2,33 @@ export default function Seventh() {
   function handleClick() {}
 
   return (
-    <div className="flex justify-center">
-      <div>
-        <div className="flex justify-center">
-          <button onClick={() => handleClick}>R</button>
-          <button onClick={() => handleClick}>Ch</button>
-          <button onClick={() => handleClick}>I</button>
-          <button onClick={() => handleClick}>Ý</button>
-        </div>
-        <div className="flex justify-center">
-          <button onClick={() => handleClick}>H</button>
-          <button onClick={() => handleClick}>O</button>
-          <button onClick={() => handleClick}>V</button>
-          <button onClick={() => handleClick}>A</button>
-        </div>
-        <div className="flex justify-center">
-          <button onClick={() => handleClick}>R</button>
-          <button onClick={() => handleClick}>P</button>
-          <button onClick={() => handleClick}>E</button>
-          <button onClick={() => handleClick}>N</button>
-        </div>
-        <div className="flex justify-center">
-          <button onClick={() => handleClick}>O</button>
-          <button onClick={() => handleClick}>T</button>
+    <>
+      <div className="flex justify-center">
+        <div>
+          <div className="flex justify-center">
+            <button onClick={() => handleClick}>R</button>
+            <button onClick={() => handleClick}>Ch</button>
+            <button onClick={() => handleClick}>I</button>
+            <button onClick={() => handleClick}>Ý</button>
+          </div>
+          <div className="flex justify-center">
+            <button onClick={() => handleClick}>H</button>
+            <button onClick={() => handleClick}>O</button>
+            <button onClick={() => handleClick}>V</button>
+            <button onClick={() => handleClick}>A</button>
+          </div>
+          <div className="flex justify-center">
+            <button onClick={() => handleClick}>R</button>
+            <button onClick={() => handleClick}>P</button>
+            <button onClick={() => handleClick}>E</button>
+            <button onClick={() => handleClick}>N</button>
+          </div>
+          <div className="flex justify-center">
+            <button onClick={() => handleClick}>O</button>
+            <button onClick={() => handleClick}>T</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
