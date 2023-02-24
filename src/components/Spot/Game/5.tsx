c99ce3133fe51efc6b932fd89a09db484d37038f
SsaiboT <tobisokola@seznam.cz>
2023-02-24 02:00:22 +0100

    desing 5

--- src/components/Spot/Game/5.tsx
+++ src/components/Spot/Game/5.tsx
@@ -1,3 +1,43 @@
 export default function Fifth() {
-  return <div>5</div>;
+  function handleClick() {}
+
+  const backgroundImage =
+    "https://cdn.discordapp.com/attachments/824638985082634250/1078473986209497178/5bg.jpg";
+
+  return (
+    <div
+      className="w-screen h-screen bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="pb-3" />
+      <div className="flex justify-center p-1 border-2 border-dashed border-orange-500 rounded-xl mx-10">
+        <div className="text-2xl text-black text-center font-bold bg-orange-300 rounded-lg p-2">
+          Kde byla tato nákaza poprvé zaznamenaná? (mapa)
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="mt-3 p-1 flex justify-center border-2 border-dashed border-orange-300 rounded-xl">
+          <input type="text" className="bg-orange-500 p-2 rounded-lg w-60 text-black font-semibold text-xl" />
+        </div>
+      </div>
+      <div className="flex justify-center mt-3">
+        <div className="p-1 border-2 border-orange-300 border-dashed rounded-xl">
+          <button
+            onClick={() => handleClick}
+            className="bg-orange-500 py-1 px-2 rounded-lg text-2xl font-bold"
+          >
+            Potvrdit
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <img
+          src="https://cdn.discordapp.com/attachments/824638985082634250/1078419067490287616/map.png"
+          alt="logo"
+          className="mt-10"
+        />
+      </div>
+    </div>
+  );
 }
