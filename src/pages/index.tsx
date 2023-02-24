6f9b945cfabcf297dff729dd65b30000f81c54b6
SsaiboT <tobisokola@seznam.cz>
2023-02-24 10:53:52 +0100

    asd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import SpotGame from "~/components/Spot";
 import dynamic from "next/dynamic";
 
 import { api } from "~/utils/api";
-import { Toaster } from "react-hot-toast";
 
 const Home: NextPage = () => {
   const getGame = api.game.getGame.useQuery();
@@ -26,8 +25,7 @@ const Home: NextPage = () => {
 
   return (
     <main className="h-screen w-screen flex-col bg-gray-200">
-      <Toaster />
-      {!getGame.data.completedSpot && <SpotGame number={1} />}
+      {!getGame.data.completedSpot && <SpotGame number={getGame.data.currentSpot.number} />}
 
       <div className="flex flex-col w-screen h-screen justify-between">
         <Mapa points={getGame.data.points} />
